b1933a3395071aa171d4727dcb95f94980d81e74
susmitha <gonepadususmithareddy917@gmail.com>
2024-07-26 12:26:27 +0530

    shell

--- create-ec2.sh
+++ create-ec2.sh
@@ -12,7 +12,7 @@ for name in ${instances[@]}; do
         instance_type="t3.micro"
     fi
     echo "Creating instance for: $name with instance type: $instance_type"
-    instance_id=$(aws ec2 run-instances --image-id ami-041e2ea9402c46c32 --instance-type $instance_type --security-group-ids sg-0fea5e49e962e81c9 --subnet-id subnet-09863c54177764565 --query 'Instances[0].InstanceId' --output text)
+    instance_id=$(aws ec2 run-instances --image-id ami-041e2ea9402c46c32 --instance-type $instance_type --security-group-ids sg-0f43d0bd748c5c8ee --subnet-id subnet-0a5e51f2c3446feea --query 'Instances[0].InstanceId' --output text)
     echo "Instance created for: $name"
 
     aws ec2 create-tags --resources $instance_id --tags Key=Name,Value=$name
